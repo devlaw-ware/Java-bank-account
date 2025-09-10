e2bf3dbae7fb49698f54cba81fd64a8c2bcf6b2f
devlaw-ware <devlaw.ware@gmail.com>
2025-09-09 21:02:16 -0300

    adição da estrutura

---
+++ src/GerenciaBancoJava.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+class ContaBancaria{
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private double saldo;
+
+    public ContaBancaria (String nome, String sobrenome, String cpf) /* criação do metodo construtor , basicamente qando ela for chamada no GerenciaBanco, ela vai ser
+    transformada em objeto e ela sendo instanciada ela pode receber um valor que eu vou informar a ela. Ou eja, em vez de eu informar um a um eu vou chamar essa classe e com ela será
+    criado o metodo construtor ( instanciar ) todo metodo construtor pecisa de parametros */
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.saldo = 0,0
+
+
+}
+    public double
+
+
+public class GerenciaBancoJava {
+    public static void main(String[] args) {
+
+
+
+        }
+    }
