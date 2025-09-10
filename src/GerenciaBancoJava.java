b733d382295c8986bcb80dd9f13f5740e7865432
devlaw-ware <devlaw.ware@gmail.com>
2025-09-10 00:09:37 -0300

    creation-struct-java-bank

--- src/GerenciaBancoJava.java
+++ src/GerenciaBancoJava.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 class ContaBancaria{
     private String nome;
@@ -7,23 +5,29 @@ class ContaBancaria{
     private String cpf;
     private double saldo;
 
-    public ContaBancaria (String nome, String sobrenome, String cpf) /* criação do metodo construtor , basicamente qando ela for chamada no GerenciaBanco, ela vai ser
+    /* criação do metodo construtor , basicamente qando ela for chamada no GerenciaBanco, ela vai ser
     transformada em objeto e ela sendo instanciada ela pode receber um valor que eu vou informar a ela. Ou eja, em vez de eu informar um a um eu vou chamar essa classe e com ela será
     criado o metodo construtor ( instanciar ) todo metodo construtor pecisa de parametros */
+
+    public ContaBancaria(String nome, String sobrenome, String cpf) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
-        this.saldo = 0,0
-
-
+        this.saldo = 0.0;
 }
-    public double
 
-
-public class GerenciaBancoJava {
-    public static void main(String[] args) {
-
-
-
+    public double consultarSaldo (){ // entre parenteses pq eu estou criando um metodo
+        return saldo;
+    }
+    public void depositar (double valor){ /* O depositar vai pegar o valor que eu informar e acrscentar junto ao meu saldo */
+        saldo += valor;
+        System.out.println("Deposito de R$ + " + valor + "realizado com sucesso");
+    }
+    public void sacar (double valor){
+        if (saldo >= valor){
+            saldo -= valor;
+            System.out.println(saque);
         }
     }
+}
+
